7ed8fb3ecc5e3b65c02d1d41ea1202977171eb88
Marya <maryammuchai@gmail.com>
2021-09-30 15:40:24 +0300

    created the details page

--- base/views.py
+++ base/views.py
@@ -1,8 +1,18 @@
+from django.db import models
 from base.models import Task
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
-class TastList(ListView):
+class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+ 
